cc56169bf9ac446cff98d52463ec76fe716a6a3e
Tagir Kadyrbecov <132221435+Tagir-Kadyrbecov@users.noreply.github.com>
2026-09-04 13:51:56 +0500

    Add files via upload

--- ChatAgent/gemini_client/gemini_service.py
+++ ChatAgent/gemini_client/gemini_service.py
@@ -1,26 +1,27 @@
-import time
+import os
 
+from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-import os
-from dotenv import load_dotenv
+
+from gemini_tools import GeminiTools
+
 load_dotenv()
 gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+
 class GeminiService:
     def __init__(self):
         self.client = genai.Client(api_key=gemini_api_key)
 
-    def chat_with_gemini(self,message: str):
-        start = time.perf_counter()
-        # generate_config = types.ChatGenerateConfig(
-        #     types.
-        # )
-        interaction = self.client.interactions.create(
+    def chat_with_gemini(self, message: str):
+        response = self.client.models.generate_content(
             model="gemini-3.5-flash",
-            input=message,
+            contents=message,
+            config=types.GenerateContentConfig(
+                tools=[
+                    GeminiTools.gemini_request_to_create_new_user
+                ]
+            )
         )
-        print(f"2. Google ответил за {time.perf_counter() - start:.2f} сек")
-        return interaction.output_text
-
-gemini_service = GeminiService()
-gemini_service.chat_with_gemini("hello")
+        return response.text
